217c1bf93b9bb6f51c69d7acb36331f6ec1b232c
Moses Karunia <moseskarunia@gmail.com>
2020-12-09 17:19:18 +0700

    params

---
+++ lib/src/data_source/params.dart
@@ -0,0 +1,29 @@
+import 'package:cleanly_architected/src/data_source/remote_data_source.dart';
+import 'package:equatable/equatable.dart';
+
+/// Query parameters of [RemoteQueryDataSource]'s [read].
+abstract class QueryParams<T> extends Equatable {}
+
+/// Use this class if you don't need to add anything to the [QueryParams]
+class NoQueryParams<T> extends QueryParams<T> {
+  @override
+  List<Object> get props => [];
+}
+
+/// Mutation parameters of [RemoteMutationDataSource]'s [create] and [update].
+abstract class MutationParams<T> extends Equatable {}
+
+/// Use this class if you don't need to add anything to the [MutationParams]
+class NoMutationParams<T> extends MutationParams<T> {
+  @override
+  List<Object> get props => [];
+}
+
+/// Deletion parameters of [RemoteMutationDataSource]'s [delete]
+abstract class DeletionParams<T> extends Equatable {}
+
+/// Use this class if you don't need to add anything to the [DeletionParams]
+class NoDeletionParams<T> extends DeletionParams<T> {
+  @override
+  List<Object> get props => [];
+}
---
+++ test/data_source/params_test.dart
@@ -0,0 +1,34 @@
+import 'package:cleanly_architected/src/data_source/params.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class _TestClass extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+void main() {
+  group('NoQueryParams', () {
+    test('props', () {
+      final q = NoQueryParams<_TestClass>();
+      expect(q.props, []);
+      expect(q, isA<QueryParams<_TestClass>>());
+    });
+  });
+
+  group('NoMutationParams', () {
+    test('props', () {
+      final q = NoMutationParams<_TestClass>();
+      expect(q.props, []);
+      expect(q, isA<MutationParams<_TestClass>>());
+    });
+  });
+
+  group('NoDeletionParams', () {
+    test('props', () {
+      final q = NoDeletionParams<_TestClass>();
+      expect(q.props, []);
+      expect(q, isA<DeletionParams<_TestClass>>());
+    });
+  });
+}
