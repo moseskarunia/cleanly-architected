170a203deca65722503b6daedc7e1b68c7f07f94
Moses Karunia <moseskarunia@gmail.com>
2020-12-11 18:17:40 +0700

    comment and add todo

--- packages/cleanly_architected_core/lib/src/repository/form_cache_repository.dart
+++ packages/cleanly_architected_core/lib/src/repository/form_cache_repository.dart
@@ -1,27 +1,29 @@
-import 'package:cleanly_architected_core/cleanly_architected_core.dart';
-import 'package:cleanly_architected_core/src/data_source/params.dart';
-import 'package:cleanly_architected_core/src/entity/equatable_entity.dart';
-import 'package:dartz/dartz.dart';
-import 'package:meta/meta.dart';
+/// TODO:
+
+// import 'package:cleanly_architected_core/cleanly_architected_core.dart';
+// import 'package:cleanly_architected_core/src/data_source/params.dart';
+// import 'package:cleanly_architected_core/src/entity/equatable_entity.dart';
+// import 'package:dartz/dartz.dart';
+// import 'package:meta/meta.dart';
 
 /// Manages form cache so the user can continue editing later.
-class FormCacheRepository<T extends EquatableEntity, U extends FormParams<T>> {
-  final LocalFormCacheDataSource<T, U> localFormCacheDataSource;
+// class FormCacheRepository<T extends EquatableEntity, U extends FormParams<T>> {
+//   final LocalFormCacheDataSource<T, U> localFormCacheDataSource;
 
-  const FormCacheRepository({@required this.localFormCacheDataSource});
+//   const FormCacheRepository({@required this.localFormCacheDataSource});
 
-  /// Removes form cache of T from LocalMutationDataSource
-  Future<Either<CleanFailure, Unit>> delete() async {
-    //
-  }
+//   /// Removes form cache of T from LocalMutationDataSource
+//   Future<Either<CleanFailure, Unit>> delete() async {
+//     //
+//   }
 
-  /// Add form params to form cache T
-  Future<Either<CleanFailure, Unit>> put({@required U params}) async {
-    //
-  }
+//   /// Add form params to form cache T
+//   Future<Either<CleanFailure, Unit>> put({@required U params}) async {
+//     //
+//   }
 
-  ///  Read form cache of T from LocalMutationDataSource if any.
-  Future<Either<CleanFailure, U>> read() async {
-    //
-  }
-}
+//   ///  Read form cache of T from LocalMutationDataSource if any.
+//   Future<Either<CleanFailure, U>> read() async {
+//     //
+//   }
+// }
