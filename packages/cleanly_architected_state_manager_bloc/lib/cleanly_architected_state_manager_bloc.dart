05c2d5589a34894d43724f1486fe264429e6f0ed
Moses Karunia <moseskarunia@gmail.com>
2020-12-18 03:38:07 +0700

    cleanly_architected_state_manager_bloc depend on core 0.0.10

--- packages/cleanly_architected_state_manager_bloc/lib/cleanly_architected_state_manager_bloc.dart
+++ packages/cleanly_architected_state_manager_bloc/lib/cleanly_architected_state_manager_bloc.dart
@@ -1,5 +1,6 @@
 library cleanly_architected_state_manager_bloc;
 
-export 'src/deletion_cubit.dart';
-export 'src/form_cubit.dart';
-export 'src/query_cubit.dart';
+export 'package:cleanly_architected_core/cleanly_architected_core.dart';
+export './src/deletion_cubit.dart';
+export './src/form_cubit.dart';
+export './src/query_cubit.dart';
